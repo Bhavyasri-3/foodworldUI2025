df59e22d6d243fc77093e85973d13cd2fa15fb96
Bhavyasri <foodworld2811@gmail.com>
2025-05-08 20:01:00 +0530

    Error solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth.service';
 export class AppComponent implements OnInit{
   title = 'Rest-App app is running!';
   isAuthenticated = false;
-  constructor( private aauthService:AuthService){
+  constructor( private authService:AuthService){
   }
  
   ngOnInit(){
